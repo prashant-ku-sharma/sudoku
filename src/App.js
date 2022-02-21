ecd1587be12d8dac0b39b5db8692a6d6bf2e9547
prashant-sharma-tws <ps142500@gmail.com>
2022-02-21 12:19:52 +0530

    THE SUDOKU SOLVER - with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useContext } from "react";
+import { BoardContext } from "./SudokuContextHelpers/Board";
+import "./App.css";
 
+let initialBoard = [];
 function App() {
+  const [board, setBoard] = useState(initialBoard);
+  const {
+    isChecking,
+    setIsChecking,
+    solvedByUser,
+    loader,
+    compareBoard,
+    getRandomBoard,
+    solve,
+  } = useContext(BoardContext);
+
+  useEffect(() => {
+    getRandomBoard().then((res) => {
+      setBoard(res.board);
+      initialBoard = res.board;
+    });
+  }, []);
+
+  const handleChange = (e, row, col) => {
+    const value = +e.target.value,
+      duplicate = JSON.parse(JSON.stringify(board));
+
+    if (value === -1 || (value >= 1 && value <= 9)) {
+      duplicate[row][col] = value;
+    }
+    setBoard(duplicate);
+  };
+
+  const check = () => compareBoard(initialBoard, board);
+  const getSolution = () => setBoard(solve(initialBoard));
+  const reset = () => {
+    setIsChecking(false);
+    setBoard(initialBoard);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <section>
+        <h1>SUDOKU</h1>
+        <p
+          style={{ display: isChecking ? "inline-block" : "none" }}
+          className={solvedByUser ? "user-result green" : "user-result red"}
         >
-          Learn React
-        </a>
-      </header>
+          {solvedByUser ? "You win!" : "You lose!"}
+        </p>
+        <p className={loader ? "loader" : "hide"}>
+          Loading<span></span>
+          <span></span>
+          <span></span>
+        </p>
+        <div className="sudoku-board">
+          {board.map((rowArray, row) => {
+            return rowArray.map((cellValue, col) => {
+              return (
+                <input
+                  type="text"
+                  value={cellValue === 0 ? "" : cellValue}
+                  onChange={(e) => handleChange(e, row, col)}
+                  disabled={cellValue !== 0}
+                  key={row + col}
+                  className="board-cell"
+                  style={{ backgroundColor: cellValue === 0 ? "" : "pink" }}
+                />
+              );
+            });
+          })}
+        </div>
+      </section>
+      <section className="btns">
+        <button onClick={check}>Check</button>
+        <button onClick={getSolution}>Solution</button>
+        <button onClick={reset}>Reset</button>
+      </section>
     </div>
   );
 }
---
+++ src/SudokuContextHelpers/Board.jsx
@@ -0,0 +1,141 @@
+import { createContext, useState } from "react";
+
+const BoardContext = createContext();
+
+const BoardContextProvider = ({ children }) => {
+  const [solvedByUser, setSolvedByUser] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [isChecking, setIsChecking] = useState(false);
+  let BackupBoard = [
+    [0, 0, 0, 9, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 5, 0, 0, 0, 9],
+    [6, 0, 0, 0, 2, 8, 0, 4, 0],
+    [0, 0, 0, 0, 0, 0, 8, 9, 0],
+    [4, 0, 0, 0, 8, 0, 1, 0, 7],
+    [0, 9, 0, 6, 1, 3, 4, 0, 0],
+    [0, 0, 0, 0, 9, 2, 7, 6, 0],
+    [0, 6, 0, 0, 3, 1, 0, 2, 4],
+    [9, 0, 2, 0, 4, 0, 0, 0, 3],
+  ];
+
+  async function getRandomBoard() {
+    try {
+      setLoader(true);
+      const response = await fetch(
+        `https://sugoku.herokuapp.com/board?difficulty=random`
+      );
+      const result = await response.json();
+      setLoader(false);
+      return result;
+    } catch (error) {
+      console.log(`Sorry, try to refresh the page!`);
+    }
+    return BackupBoard;
+  }
+
+  // sudoku-solver
+  let storeAllResults;
+  function sudokuSolver(board, row, col) {
+    if (row === board.length) {
+      storeAllResults.push(
+        board.map((row) => {
+          return row.join(" ").split(" ").map(Number);
+        })
+      );
+      return 0;
+    }
+
+    let nxt_row = 0,
+      nxt_col = 0;
+    if (col === 8) {
+      nxt_row = row + 1;
+      nxt_col = 0;
+    } else {
+      nxt_row = row;
+      nxt_col = col + 1;
+    }
+
+    if (board[row][col] !== 0) {
+      sudokuSolver(board, nxt_row, nxt_col);
+    } else {
+      for (let value = 1; value <= 9; value++) {
+        if (check(board, row, col, value)) {
+          board[row][col] = value;
+          sudokuSolver(board, nxt_row, nxt_col);
+          board[row][col] = 0;
+        }
+      }
+    }
+
+    return false;
+  }
+  // sudoku-value checker
+  function check(board, row, col, value) {
+    // vertically
+    for (let x = 0; x < 9; x++) {
+      if (board[x][col] === value) return false;
+    }
+    // horizontally
+    for (let x = 0; x < 9; x++) {
+      if (board[row][x] === value) return false;
+    }
+    // grid 3x3
+    let grid_row = Math.floor(row / 3) * 3;
+    let grid_col = Math.floor(col / 3) * 3;
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        if (board[grid_row + x][grid_col + y] === value) return false;
+      }
+    }
+
+    return true;
+  }
+
+  function compareBoard(initialBoard, board) {
+    let isSolved = false,
+      becomeFalse = true;
+    setIsChecking(becomeFalse);
+    storeAllResults = [];
+    sudokuSolver(initialBoard, 0, 0);
+
+    for (let x = 0; x < storeAllResults.length; x++) {
+      let res = storeAllResults[x],
+        flag = 0;
+      res.forEach((row, index) => {
+        if (row.join(" ") === board[index].join(" ")) {
+          flag++;
+        }
+      });
+      if (flag === 9) {
+        isSolved = true;
+        becomeFalse = false;
+        break;
+      }
+      if (x === storeAllResults.length - 1) becomeFalse = false;
+    }
+
+    setSolvedByUser(isSolved);
+  }
+
+  function solve(initialBoard) {
+    storeAllResults = [];
+    sudokuSolver(initialBoard, 0, 0);
+    return storeAllResults[0];
+  }
+
+  const value = {
+    solvedByUser,
+    loader,
+    setLoader,
+    isChecking,
+    setIsChecking,
+    getRandomBoard,
+    solve,
+    compareBoard,
+  };
+  return (
+    <BoardContext.Provider value={value}>{children}</BoardContext.Provider>
+  );
+};
+
+export { BoardContext, BoardContextProvider };
